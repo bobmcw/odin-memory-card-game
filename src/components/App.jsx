79f2f76bdaddc6e59fe9d7853e3f427a80a2fd07
bobmcw <jasiekz2014@gmail.com>
2025-02-07 20:27:18 +0100

    created a function to fetch a random pokemon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const api = "https://pokeapi.co/api/v2/pokemon/"
+  const [pokemons, setPokemons] = useState([])
+  async function getRandomPokemon() {
+   try {
+    const response = await fetch(api + Math.floor(Math.random()*150));
+    if(!response.ok){
+      throw new Error("response status: " + response.status);
+    }
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+   } 
+  useEffect(() => {})
   return (
     <>
+    <button onClick={() => getRandomPokemon()}></button>
     </>
   )
 }
