942b3d5655eb0a1ff2202d3129d84512bec5085b
bobmcw <jasiekz2014@gmail.com>
2025-02-08 13:16:22 +0100

    aded an effect to log the fetch result

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,34 @@
-import { useEffect, useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import '../styles/App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "../styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const api = "https://pokeapi.co/api/v2/pokemon/"
-  const [pokemons, setPokemons] = useState([])
+  const [count, setCount] = useState(0);
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+  const [pokemons, setPokemons] = useState([]);
   async function getRandomPokemon() {
-   try {
-    const response = await fetch(api + Math.floor(Math.random()*150));
-    if(!response.ok){
-      throw new Error("response status: " + response.status);
+    try {
+      const response = await fetch(api + Math.floor(Math.random() * 150));
+      if (!response.ok) {
+        throw new Error("response status: " + response.status);
+      }
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } catch (error) {
+      console.error(error.message);
+      return null
     }
-    const json = await response.json();
-    console.log(json);
-  } catch (error) {
-    console.error(error.message);
   }
-   } 
-  useEffect(() => {})
+  useEffect(() => {
+    console.log(pokemons);
+  }, [pokemons]);
   return (
     <>
-    <button onClick={() => getRandomPokemon()}></button>
+      <button onClick={() => getRandomPokemon()}></button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
