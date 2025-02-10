da5f07296ae94d2cb41d88a4081bc74351ae6bb7
bobmcw <jasiekz2014@gmail.com>
2025-02-10 11:57:32 +0100

    extracted the suffle function to be seperate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,13 +18,17 @@ function App() {
       return null
     }
   }
-  async function generatePokemons(count,rangeStart,rangeEnd){
-      const idArray = Array.from({length: rangeEnd - rangeStart + 1}, (_, i) => i+rangeStart)
+  function shuffle(arr){
       //Fisher-Yates Shuffle
-      for (let i = idArray.length - 1; i > 0; i--){
+      for (let i = arr.length - 1; i > 0; i--){
         const j = Math.floor(Math.random() * (i+1));
-        [idArray[i], idArray[j]] = [idArray[j], idArray[i]];
+        [arr[i], arr[j]] = [arr[j], arr[i]];
       }
+      return arr
+  }
+  async function generatePokemons(count,rangeStart,rangeEnd){
+      let idArray = Array.from({length: rangeEnd - rangeStart + 1}, (_, i) => i+rangeStart)
+      idArray = shuffle(idArray);
       let promises = idArray.slice(0,count);
       promises = promises.map((num) => getPokemonWithID(num));
       const results = await Promise.all(promises)
