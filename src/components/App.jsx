698716a065272debc5ae72076d8d70becfe1b394
bobmcw <jasiekz2014@gmail.com>
2025-02-10 12:29:55 +0100

    added functionality to start a game

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
 import CardContainer from "./CardContainer";
 import "../styles/App.css";
-
-function startGame(){
-
-}
-
+import { useState } from "react";
 function App() {
+  const [dif, setDif] = useState(0);
+  const [gen, setGen] = useState(1);
+  const [game,setGame] = useState(false);
   return (
     <>
-      <button onClick={() => startGame}>Go!</button>
+      <div className="selectItem">
+        <label htmlFor="difficulty">difficulty</label>
+        <select name="difficulty" id="difficulty" onChange={(e) => setDif(e.target.value)}>
+          <option value="0">easy</option>
+          <option value="1">medium</option>
+          <option value="2">hard</option>
+        </select>
+      </div>
+      <div className="selectItem">
+        <label htmlFor="generation">generation</label>
+        <select name="generation" id="generation" onChange={(e) => setGen(e.target.value)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select>
+      </div>
+      { game ? "" : <button onClick={() => setGame(true)}>Go!</button>}
+      {game ? <CardContainer /> : ""}
     </>
   );
 }
