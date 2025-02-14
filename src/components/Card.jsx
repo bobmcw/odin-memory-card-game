2499edaf545efd20a68a0ccbd336159479706fed
bobmcw <jasiekz2014@gmail.com>
2025-02-14 11:21:59 +0100

    added a loading annimation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react"
+import { PulseLoader } from "react-spinners"
 function Card({pokemonName, spriteURL, handleClick, pokemonId}){
+    const [loaded, setLoaded] = useState(false)
     return(
         <div className="card" onClick={() => handleClick(pokemonId)}>
-            <img src={spriteURL} alt={pokemonName} />
+            {!loaded ? <PulseLoader size={8}/> : ""}
+            <img src={spriteURL} alt={pokemonName} onLoad={() => setLoaded(true)}/>
             <p>{pokemonName}</p>
         </div>
     )
