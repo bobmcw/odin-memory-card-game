ccc01f6824370bc6a4c78c31b042d7321bf2ec3a
bobmcw <jasiekz2014@gmail.com>
2025-02-18 21:17:58 +0100

    added aria labels and tabindex to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import "../styles/Card.css"
 function Card({pokemonName, spriteURL, handleClick, pokemonId}){
     const [loaded, setLoaded] = useState(false)
     return(
-        <div className="card" onClick={() => handleClick(pokemonId)}>
+        <div className="card" tabIndex={1} aria-label={"card " + pokemonName} onClick={() => handleClick(pokemonId)}>
             {!loaded ? <PulseLoader size={8}/> : ""}
             <img src={spriteURL} alt={pokemonName} onLoad={() => setLoaded(true)}/>
             <p className="name">{pokemonName}</p>
